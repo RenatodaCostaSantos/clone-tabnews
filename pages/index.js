5edd22c19aef5140120d8844364578ba0be827ee
RenatodaCostaSantos <kikuisso@gmail.com>
2025-04-04 11:55:41 -0300

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Dani, seu sorriso aquece meu coração. Obrigado, muito obrigado!</h1>
+    return <h1>Saudades, gatinha! Quero jantar com vc. Quero jantar, vc. </h1>
 }
 
 export default Home;

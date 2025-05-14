08a90405baa95f61799d7e532e05547bdc9c1f01
RenatodaCostaSantos <kikuisso@gmail.com>
2025-05-14 14:05:57 -0300

    adiciona pagina inicial do site

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 function Home() {
   return (
-    <h1>
-      Saudades do quarteto fantastico! Quando precisarem, estou aqui, aprendendo
-      a fazer sites e comendo doce de leite. Abraço!{" "}
-    </h1>
+    <html lang="pt-br">
+      <body>
+        <h1>Propósito</h1>
+        <p>
+          Unir a comunidade de escaladores e compartilhar a alegria de
+          entusiastas da escalada em São Bento do Sapucaí.
+        </p>
+
+        <blockquote>
+          “A felicidade só é real quando é compartilhada.”
+        </blockquote>
+
+        <h2>Perfil</h2>
+        <p>
+          Escaladores de todos os níveis são bem-vindos. Queremos ouvir sua
+          história no processo de se desafiar em uma escalada, seja do nível que
+          for. Queremos saber:
+        </p>
+
+        <ul>
+          <li>Por que essa via?</li>
+          <li>Qual foi o seu maior desafio nessa cadena?</li>
+          <li>Como foi o processo até esse dia?</li>
+          <li>Quais amigos você gostaria de celebrar nesse dia?</li>
+          <li>O que vem agora?</li>
+        </ul>
+      </body>
+    </html>
   );
 }
 

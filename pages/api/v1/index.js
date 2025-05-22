e62bc599d4bce6dec1df5377ef2383323228bc93
Renato <renato.costa87@gmail.com>
2025-05-22 12:13:11 -0300

    realoca a pagina inicial deletada por engano

---
+++ pages/api/v1/index.js
@@ -0,0 +1,42 @@
+function Home() {
+  return (
+    <main lang="pt-br">
+      <h1>Propósito</h1>
+      <p>
+        Unir a comunidade de escaladores e compartilhar a alegria de entusiastas
+        da escalada em São Bento do Sapucaí.
+      </p>
+
+      <strong>
+        “A felicidade só é real quando é compartilhada.” - Henry Thoreau
+      </strong>
+
+      <h2>Perfil</h2>
+      <p>
+        Escaladores de todos os níveis são bem-vindos. Queremos ouvir sua
+        história no processo de se desafiar em uma escalada, seja do nível que
+        for. Queremos saber:
+      </p>
+
+      <ul>
+        <li>Por que essa via?</li>
+        <li>Qual foi o seu maior desafio nessa cadena?</li>
+        <li>Como foi o processo até esse dia?</li>
+        <li>Quais amigos você gostaria de celebrar nesse dia?</li>
+        <li>O que vem agora?</li>
+      </ul>
+
+      <h2>Contato</h2>
+      <p>Mandou uma via em São Bento do Sapucaí?</p>
+
+      <p>
+        Nos envie um email com fotos e as respostas das perguntas acima que
+        iremos publicar uma mini reportagem sobre sua cadena aqui!
+      </p>
+
+      <strong>cadenasaobento@gmail.com</strong>
+    </main>
+  );
+}
+
+export default Home;

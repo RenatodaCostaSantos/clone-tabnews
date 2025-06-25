fc0caaa3b68e24d64d150fd40ccede2b338f96d6
Renato <renato.costa87@gmail.com>
2025-06-25 15:36:03 -0300

    finaliza o endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from 'infra/database.js';
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+
+  const {rows: versionRows} = await database.query("SHOW server_version;");
+  const {rows: maxConnectionRows} = await database.query("SHOW max_connections;");
+  const  databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsRows = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const version = versionRows[0].server_version;
+  const maxConnections = parseInt(maxConnectionRows[0].max_connections, 10);
+  const activeConnections = parseInt(activeConnectionsRows.rows[0].count, 10);
+
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: maxConnections,
+        active_connections: activeConnections
+    }
+  }});
 }
 
-export default status;
+export default status;  
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,36 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const { dependencies, updated_at} = await response.json();  
+  const pgVersion = dependencies.database.version;
+  const maxConnections = dependencies.database.max_connections;
+
+
+  expect(updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(updated_at).toISOString();
+  expect(updated_at).toEqual(parsedUpdatedAt);
+
+  expect(pgVersion).toBeDefined();
+  expect(typeof pgVersion).toBe("string");
+  expect(pgVersion.length).toBeGreaterThan(0);
+
+  expect(maxConnections).toBeDefined();
+  expect(typeof maxConnections).toBe("number");
+  expect(maxConnections).toBeGreaterThan(0)
 });
+
+test("Obtain max connections from /api/v1/status ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const { dependencies } = await response.json();  
+  const activeConnections = dependencies.database.active_connections;
+
+  expect(activeConnections).toBeDefined();
+  expect(typeof activeConnections).toBe("number");
+  expect(activeConnections).toBe(1)
+});
+
+
+
+
